83ae2176d695c6e5296e57fdf155e3772d3cf461
Mlipphardt <matthewlipphardt@gmail.com>
2020-04-11 20:41:54 -0400

    Initial files and package requirements.

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var axios = require("axios");
+const mongoose = require("mongoose");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+
+app.set("view engine", "handlebars");
+
+var routes = require("./routes/routes");
+
+app.use(routes);
+
+app.listen(PORT, function () {
+  console.log("Sever live and listening on PORT " + PORT + "!");
+});
