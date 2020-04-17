b83f95bdc6250e4fcad85c3df90d3b60e81f4b57
Mlipphardt <matthewlipphardt@gmail.com>
2020-04-17 18:15:35 -0400

    Clients can now unfavorite articles

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,28 @@
 $(document).on("click", ".favorite-button", function () {
   let id = $(this).attr("data-id");
-  $.ajax({
-    type: "PUT",
-    url: "/favorites/" + id,
-    data: { favorite: true },
-  }).then(function (err, res) {
-    if (err) {
-      console.log(err);
-    }
-    console.log(res);
-  });
+  let favorite = $(this).attr("data-favorited");
+  console.log(favorite);
+  if (favorite) {
+    $.ajax({
+      type: "PUT",
+      url: "/favorites/" + id,
+      data: { favorite: false },
+    }).then(function (err, res) {
+      if (err) {
+        console.log(err);
+      }
+      console.log(res);
+    });
+  } else {
+    $.ajax({
+      type: "PUT",
+      url: "/favorites/" + id,
+      data: { favorite: true },
+    }).then(function (err, res) {
+      if (err) {
+        console.log(err);
+      }
+      console.log(res);
+    });
+  }
 });
