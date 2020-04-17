a6c0ebd1e2d31cb6c9fc3a52cfb3424028e54929
Mlipphardt <matthewlipphardt@gmail.com>
2020-04-17 12:47:42 -0400

    Models created for articles and notes

---
+++ models/Article.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note",
+  },
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
---
+++ models/Note.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var NoteSchema = new Schema({
+  title: String,
+  body: String,
+});
+
+var Note = mongoose.model("Note", NoteSchema);
+
+module.exports = Note;
